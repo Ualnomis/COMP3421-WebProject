37a873a47788c1820e8ccfcea5525e44e48d0c4e
Ualnomis <ualnamnuhc@gmail.com>
2023-04-10 16:19:59 +0800

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+$pageTitle = "Index";
+$styles = "";
+
+// Include the template file
+include('../templates/header.php');
+?>
+<?php
+// Include the template file
+include('../templates/navbar.php');
+?>
+<div class="page-wrapper">
+
+</div>
+<?php
+$scripts = "";
+// Include the template file
+include('../templates/footer.php');
+?>
