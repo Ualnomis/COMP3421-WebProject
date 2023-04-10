a584fa1eb20660949bbefb089b343191b60d24d5
Ualnomis <ualnamnuhc@gmail.com>
2023-04-10 16:18:28 +0800

    Create header.php

---
+++ templates/header.php
@@ -0,0 +1,18 @@
+<?php
+include_once '../config/db_connection.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>
+        <?php echo $pageTitle; ?>
+    </title>
+    <link href="../assets/libs/tabler/css/tabler.min.css" rel="stylesheet">
+    <?php echo $styles; ?>
+</head>
+
+<body class="theme-dark">
