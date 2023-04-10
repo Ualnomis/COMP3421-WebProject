f83c8c3db824ddd097ddab6332ec0f1016887707
Ualnomis <ualnamnuhc@gmail.com>
2023-04-10 16:19:50 +0800

    feat: Create `User` class with basic properties and methods

---
+++ includes/user.php
@@ -0,0 +1,52 @@
+<?php
+class User {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function register($username, $email, $password, $role) {
+        // Check if email already exists in database
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            // Email already exists, return false
+            return false;
+        }
+    
+        // Email does not exist, insert new record
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $this->conn->prepare("INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $username, $email, $hashed_password, $role);
+        $result = $stmt->execute();
+        $stmt->close();
+        return $result;
+    }    
+
+    public function login($email, $password) {
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            if (password_verify($password, $user['password'])) {
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['user_name'] = $user['username'];
+                $_SESSION['user_icon'] = $user['icon_url'];
+                $_SESSION['role'] = $user['role'];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function logout() {
+        session_destroy();
+    }
+}
+
+?>
