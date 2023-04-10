e571311aa434129fc1b808a2bc22179017f441d4
Ualnomis <ualnamnuhc@gmail.com>
2023-04-10 20:00:15 +0800

    Create page-wrapper-start.inc.php

---
+++ includes/page-wrapper-start.inc.php
@@ -0,0 +1,15 @@
+
+
+<div class="page-wrapper">
+    <!-- Page header -->
+    <div class="page-header d-print-none">
+        <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                <div class="col">
+                    <h2 class="page-title">
+                        <?php echo $page_title; ?>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
