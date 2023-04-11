b12ce763837834978e46eea44426b6e6321dfaa6
Ualnomis <ualnamnuhc@gmail.com>
2023-04-11 17:29:56 +0800

    Create cart.class.php

---
+++ classes/cart.class.php
@@ -0,0 +1,114 @@
+<?php
+
+class Cart
+{
+
+    private $conn;
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    // Select cart data for a given user ID
+    public function select($user_id)
+    {
+        $stmt = $this->conn->prepare("
+            SELECT *
+            FROM shopping_cart
+            WHERE user_id = ?
+        ");
+        $stmt->bind_param('i', $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $cart = $result->fetch_assoc();
+        $stmt->close();
+        return $cart;
+    }
+
+    // Insert a new cart for a given user ID
+    public function insert($user_id)
+    {
+        $stmt = $this->conn->prepare("
+            INSERT INTO shopping_cart (user_id)
+            VALUES (?)
+        ");
+        $stmt->bind_param('i', $user_id);
+        $stmt->execute();
+        $cart_id = $stmt->insert_id;
+        $stmt->close();
+        return $cart_id;
+    }
+
+    // Update the quantity of a cart item
+    public function updateCartItem($cart_item_id, $quantity)
+    {
+        $stmt = $this->conn->prepare("
+            UPDATE shopping_cart_items
+            SET quantity = ?
+            WHERE id = ?
+        ");
+        $stmt->bind_param('ii', $quantity, $cart_item_id);
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    // Delete a cart item
+    public function deleteCartItem($cart_item_id)
+    {
+        $stmt = $this->conn->prepare("
+            DELETE FROM shopping_cart_items
+            WHERE id = ?
+        ");
+        $stmt->bind_param('i', $cart_item_id);
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    // Add a new item to the cart
+    public function addItem($cart_id, $product_id, $quantity)
+    {
+        $stmt = $this->conn->prepare("
+            INSERT INTO shopping_cart_items (cart_id, product_id, quantity)
+            VALUES (?, ?, ?)
+        ");
+        $stmt->bind_param('iii', $cart_id, $product_id, $quantity);
+        $stmt->execute();
+        $cart_item_id = $stmt->insert_id;
+        $stmt->close();
+        return $cart_item_id;
+    }
+
+    // Get cart items for a given cart ID
+    public function getCartItems($cart_id)
+    {
+        $stmt = $this->conn->prepare("
+            SELECT shopping_cart_items.*, products.name, products.price
+            FROM shopping_cart_items
+            INNER JOIN products ON shopping_cart_items.product_id = products.id
+            WHERE cart_id = ?
+        ");
+        $stmt->bind_param('i', $cart_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $cart_items = $result->fetch_all(MYSQLI_ASSOC);
+        $stmt->close();
+        return $cart_items;
+    }
+
+    // Count the number of items in a given cart
+    public function countItems($cart_id)
+    {
+        $stmt = $this->conn->prepare("
+            SELECT COUNT(*)
+            FROM shopping_cart_items
+            WHERE cart_id = ?
+        ");
+        $stmt->bind_param('i', $cart_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $count = $result->fetch_row()[0];
+        $stmt->close();
+        return $count;
+    }
+}
