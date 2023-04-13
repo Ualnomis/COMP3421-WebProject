8022d734f856506d554d9c5cb6c0907bfc398ea0
Ualnomis <ualnamnuhc@gmail.com>
2023-04-14 01:50:12 +0800

    feat: Add role check

--- includes/update-cart.inc.php
+++ includes/update-cart.inc.php
@@ -2,6 +2,16 @@
 include_once '../config/db_connection.php';
 require_once '../classes/cart.class.php';
 
+if (!isset($_SESSION['user_id'])) {
+    echo '<script>window.location.replace("../public/");</script>';
+    exit;
+} else if ($_SESSION['role'] === 'seller') {
+    echo '<script>window.location.replace("../public/");</script>';
+    exit;
+} else if ($_SESSION['role'] === 'buyer') {
+
+}
+
 $cart = new Cart($conn);
 
 if (isset($_SESSION['role'], $_SESSION['user_id']) && $_SESSION['role'] === 'buyer') {
