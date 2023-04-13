a6e40747d555760ca869f5ef9bd35b73b5cd60ca
Ualnomis <ualnamnuhc@gmail.com>
2023-04-14 01:47:05 +0800

    feat: Add role check for `delete-cart-item.inc.php`

--- includes/delete-cart-item.inc.php
+++ includes/delete-cart-item.inc.php
@@ -3,8 +3,11 @@ require_once '../config/db_connection.php';
 require_once '../classes/cart.class.php';
 
 if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
-    exit;
+    echo '<script>window.location.replace("../public/");</script>';
+} else if ($_SESSION['role'] === 'seller') {
+    echo '<script>window.location.replace("../public/");</script>';
+} else if ($_SESSION['role'] === 'buyer') {
+
 }
 
 $cart = new Cart($conn);
