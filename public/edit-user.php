67e1c100b9b57eb92e4f877f4143be7ad15f896d
Ualnomis <ualnamnuhc@gmail.com>
2023-04-13 22:08:25 +0800

    chore: Add background to each form/table

--- public/edit-user.php
+++ public/edit-user.php
@@ -3,7 +3,7 @@
 $title = "Edit User";
 $styles = <<<HTML
 HTML;
-$page_title = "Edit User";
+$page_title = "";
 
 include_once('../includes/header.inc.php');
 include_once('../includes/navbar.inc.php');
@@ -14,7 +14,7 @@ $user_role = $_SESSION['role'];
 $user = new User($conn);
 
 
-if($user_role == "seller"){
+if ($user_role == "seller") {
     if (isset($_GET['userid'])) {
         $user_id = $_GET['userid'];
         $user_data = $user->selectUserByID($user_id);
@@ -25,7 +25,7 @@ if($user_role == "seller"){
         header("Location: team-member.php");
         exit();
     }
-}else{
+} else {
     $user_id = $_SESSION['user_id'];
     $user_data = $user->selectUserByID($user_id);
 
@@ -36,13 +36,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = trim($_POST['password']);
     $confirm_password = trim($_POST['confirm_password']);
     $user_id = trim($_POST['userid']);
-    
-    if(isset($_POST['role'])){
+
+    if (isset($_POST['role'])) {
         $role = $_POST['role'];
-    }else{
+    } else {
         $role = $user_data['role'];
     }
-    if(empty($email) || empty($password) || empty($confirm_password)){
+    if (empty($email) || empty($password) || empty($confirm_password)) {
         $error = "All fields are required";
     } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $error = "Invalid email format";
@@ -51,60 +51,69 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         $user->updateUser($email, $role, $password, $user_id);
         $error = $user_id;
-        if($user_role == 'buyer'){
+        if ($user_role == 'buyer') {
             header('Location: index.php');
             exit();
-        }else {
+        } else {
             header("Location: team-member.php");
             exit();
         }
     }
-} 
+}
 ?>
 
 <div class="container-xl mt-3">
-    <div class="row">
-        <div class="col-12">
-            <?php if (isset($error)) { ?>
-                <div class="alert alert-danger" role="alert">
-                    <?= $error ?>
+    <div class="card">
+        <div class="card-body">
+            <h1 class="card-title">Edit User</h1>
+            <div class="row row-cards">
+                <div class="col-12">
+                    <?php if (isset($error)) { ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?= $error ?>
+                        </div>
+                    <?php } ?>
+                    <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+                        <div class="form-group pt-2">
+                            <label for="id">User ID:</label>
+                            <input type="text" class="form-control" id="id" name="id" value="<?= $user_data['id'] ?>"
+                                disabled>
+                            <input type="hidden" name="userid" value="<?= $user_id ?>">
+                        </div>
+                        <div class="form-group pt-2">
+                            <label for="email">Email:</label>
+                            <input type="email" class="form-control" id="email" name="email"
+                                value="<?= $user_data['email'] ?>">
+                        </div>
+                        <?php if ($user_role == "seller"): ?>
+                            <div class="form-group pt-3">
+                                <label for="role">Role:</label>
+                                <select class="form-control" id="role" name="role">
+                                    <option value="buyer" <?= ($user_data['role'] == 'buyer') ? 'selected' : '' ?>>Buyer
+                                    </option>
+                                    <option value="seller" <?= ($user_data['role'] == 'seller') ? 'selected' : '' ?>>Seller
+                                    </option>
+                                </select>
+                            </div>
+                        <?php endif; ?>
+                        <div class="form-group pt-3">
+                            <label for="password">Password:</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                        <div class="form-group pt-3">
+                            <label for="confirm_password">Confirm Password:</label>
+                            <input type="password" class="form-control" id="confirm_password" name="confirm_password"
+                                required>
+                        </div>
+                        <div class="pt-3">
+                            <button type="submit" class="btn btn-outline-dark">Save</button>
+                        </div>
+                    </form>
                 </div>
-            <?php } ?>
-            <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" >
-                <div class="form-group pt-2">
-                    <label for="id">User ID:</label>
-                    <input  type="text" class="form-control" id="id" name="id" value="<?= $user_data['id'] ?>" disabled>
-                    <input type="hidden" name="userid" value="<?= $user_id ?>">
-                </div>
-                <div class="form-group pt-2">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" name="email" value="<?= $user_data['email'] ?>">
-                </div>
-                <?php if($user_role == "seller" ): ?>
-                <div class="form-group pt-3" >
-                    <label for="role">Role:</label>
-                    <select class="form-control" id="role" name="role">
-                        <option value="buyer" <?= ($user_data['role'] == 'buyer') ? 'selected' : '' ?>>Buyer</option>
-                        <option value="seller" <?= ($user_data['role'] == 'seller') ? 'selected' : '' ?>>Seller</option>
-                    </select>
-                </div>
-                <?php endif; ?>
-                <div class="form-group pt-3" >
-                    <label for="password">Password:</label>
-                    <input type="password" class="form-control" id="password" name="password" required>
-                </div>
-                <div class="form-group pt-3" >
-                    <label for="confirm_password">Confirm Password:</label>
-                    <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
-                </div>
-                <div class="pt-3">
-                  <button type="submit" class="btn btn-outline-dark">Save</button>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
-
 <?php
 include_once('../includes/page-wrapper-end.inc.php');
 $scripts = "";
--- public/team-member.php
+++ public/team-member.php
@@ -2,65 +2,69 @@
 $title = "Users";
 $styles = <<<HTML
 HTML;
-$page_title = "Users";
+$page_title = "";
 
 include_once('../includes/header.inc.php');
 include_once('../includes/navbar.inc.php');
 require_once('../classes/user.class.php');
 $user = new User($conn);
-$users = $user -> getAllUsers();
+$users = $user->getAllUsers();
 ?>
 
 <div class="container-xl mt-3">
-    <div class="row">
-        <div class="col-12">
-            <h1>Users</h1>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <!-- <th>Icon</th> -->
-                        <th>Role</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php if (count($users) > 0): ?>
-                        <?php foreach ($users as $user): ?>
+    <div class="card">
+        <div class="card-body">
+            <h1 class="card-title">Users</h1>
+            <div class="row row-cards">
+                <div class="col-12">
+                    <table class="table">
+                        <thead>
                             <tr>
-                                <td>
-                                    <?= $user['id'] ?>
-                                </td>
-                                <td>
-                                    <?= $user['username'] ?>
-                                </td>
-                                <td>
-                                    <?= $user['email'] ?>
-                                </td>
-                                <!-- <td>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <!-- <th>Icon</th> -->
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if (count($users) > 0): ?>
+                                <?php foreach ($users as $user): ?>
+                                    <tr>
+                                        <td>
+                                            <?= $user['id'] ?>
+                                        </td>
+                                        <td>
+                                            <?= $user['username'] ?>
+                                        </td>
+                                        <td>
+                                            <?= $user['email'] ?>
+                                        </td>
+                                        <!-- <td>
                                     <img src="<?= $user['icon'] ?>" width="50" height="50">
                                 </td> -->
-                                <td>
-                                    <?= $user['role'] ?>
-                                </td>
-                                <td>
-                                <a href="edit-user.php?userid=<?= $user['id'] ?>"><i class="fa-solid fa-magnifying-glass"></i></a>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <tr>
-                            <td colspan="5">No users found.</td>
-                        </tr>
-                    <?php endif; ?>
-                </tbody>
-            </table>
+                                        <td>
+                                            <?= $user['role'] ?>
+                                        </td>
+                                        <td>
+                                            <a href="edit-user.php?userid=<?= $user['id'] ?>"><i
+                                                    class="fa-solid fa-magnifying-glass"></i></a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <tr>
+                                    <td colspan="5">No users found.</td>
+                                </tr>
+                            <?php endif; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-
 <?php
 include_once('../includes/page-wrapper-end.inc.php');
 $scripts = "";
