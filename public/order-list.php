71f4605512d461a9c914ac0b16e336bf49bc3e99
Ualnomis <ualnamnuhc@gmail.com>
2023-04-13 23:53:43 +0800

    fix: Resolve btn-close display bug

--- public/order-list.php
+++ public/order-list.php
@@ -148,7 +148,7 @@ HTML;
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Order Details</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body" id="orderDetails">
                         <!-- Order details will be populated here -->
