37fa4d9fae8f057861ec5cbaa1f0ee865a75f42c
Ualnomis <ualnamnuhc@gmail.com>
2023-04-14 01:46:27 +0800

    Update clear-cart.php

--- includes/clear-cart.php
+++ includes/clear-cart.php
@@ -3,7 +3,7 @@ require_once '../config/db_connection.php';
 require_once '../classes/cart.class.php';
 
 if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
+    header('Location: ../public/');
     exit;
 }
 
